c9644fa1e947a8fe6bb62db760d9c5e4b6e8eab7
zakariamohamed123 <149509331+zakariamohamed123@users.noreply.github.com>
2023-11-23 20:34:57 +0300

    Update CategoryFilter.js

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,25 @@
-import React from "react";
 
-function CategoryFilter() {
+import React from 'react';
+
+const CategoryFilter = ({ categories, selectedCategory, onCategorySelect }) => {
+  const handleCategoryClick = (category) => {
+    onCategorySelect(category);
+  };
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={category === selectedCategory ? 'selected' : ''}
+          onClick={() => handleCategoryClick(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
-}
+};
 
 export default CategoryFilter;
