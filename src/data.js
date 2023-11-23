bfe788efbcec2f70c1ac22e0e494fb9077e266c7
zakariamohamed123 <149509331+zakariamohamed123@users.noreply.github.com>
2023-11-23 20:32:08 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,32 +1,34 @@
 export const CATEGORIES = ["All", "Code", "Food", "Money", "Misc"];
 
 export const TASKS = [
-  {
+  { id:1,
     text: "Buy rice",
     category: "Food",
   },
-  {
+  {  id:2,
     text: "Save a tenner",
     category: "Money",
   },
-  {
+  {  id:3,
     text: "Build a todo app",
     category: "Code",
   },
-  {
+  { id:4,
     text: "Build todo API",
     category: "Code",
   },
-  {
+  { id:5,
     text: "Get an ISA",
     category: "Money",
   },
-  {
+  { id:6,
     text: "Cook rice",
     category: "Food",
   },
-  {
+  { id:7,
     text: "Tidy house",
     category: "Misc",
   },
 ];
+export const getNextId = ((id) => () => ++id)(3);
+
