bf004d21e56a3e7a63e8df4b49d3687cef0b6285
zakariamohamed123 <149509331+zakariamohamed123@users.noreply.github.com>
2023-11-23 20:35:24 +0300

    Update NewTaskForm.js

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,21 +1,56 @@
-import React from "react";
+import { useState } from 'react';
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState('');
+  const [category, setCategory] = useState(categories[0]);
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTask = { text, category };
+    onTaskFormSubmit(newTask);
+    setText('');
+    setCategory(categories[0]);
+  };
+
+  const categoryOptions = categories
+    .filter((category) => category !== 'All')
+    .map((category) => (
+      <option key={category} value={category}>
+        {category}
+      </option>
+    ));
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
-      <input type="submit" value="Add task" />
+    <form onSubmit={handleSubmit} className="new-task-form">
+      <label htmlFor="text-input">Task:</label>
+      <input
+        id="text-input"
+        type="text"
+        value={text}
+        onChange={handleTextChange}
+        required
+      />
+
+      <label htmlFor="category-select">Category:</label>
+      <select
+        id="category-select"
+        value={category}
+        onChange={handleCategoryChange}
+        required
+      >
+        {categoryOptions}
+      </select>
+
+      <button type="submit">Add Task</button>
     </form>
   );
 }
-
-export default NewTaskForm;
+export default NewTaskForm
